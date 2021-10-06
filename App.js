001ec11f51877fd307d7775e4582b78925012320
liamlebenon <82100110+liamlebenon@users.noreply.github.com>
2021-10-06 13:02:53 +0100

    Add user routing

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { Header } from './components/Header/Header';
 import { PostList } from './components/PostList/PostList'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { UserProfile } from './components/UserProfile/UserProfile';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-        <Header />
-        <PostList />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <div class='body-container'>
+            <Switch>
+              <Route path='/' exact component={PostList} />
+              <Route path='/users/:username' component={UserProfile} />
+            </Switch>
+          </div>
+      </div>
+    </Router>
   );
 }
 
--- components/PostList/PostList.js
+++ components/PostList/PostList.js
@@ -1,32 +1,46 @@
 import './PostList.css'
 import { Post } from '../Post/Post';
 import { useEffect, useState } from 'react';
+import { SearchBar } from '../SearchBar/SearchBar'
 
 export const PostList = () => {
-    
+
     useEffect(() => {
-        fetchPosts();
-      }, []);
-      
-      const [posts, setPosts] = useState([]);
-      const fetchPosts = async () => {
-        const data = await fetch(
-          'https://www.reddit.com/r/funny.json'
-        );
-        const response = await data.json();
-        let posts = await response.data.children;
-        console.log(posts);
-        setPosts(posts);
-      }
+      fetchPosts();
+    }, []);
+    
+    
+    const [posts, setPosts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('popular');
+
+    const fetchPosts = async () => {
+      const data = await fetch(
+        `https://www.reddit.com/r/${searchTerm}.json`
+      );
+      const response = await data.json();
+      let posts = await response.data.children;
+      setPosts(posts);
+    }
+    
+    const handleCallback = (searchData) => {
+      setSearchTerm(searchData)
+      fetchPosts();
+    }
 
     return(
         <div className='postlist'>
+          <SearchBar callBack={handleCallback}/>
             {posts.map(post => (
                 <div className='post'>
                     <Post title={post.data.title}
                           author={post.data.author}
                           src={post.data.url}
-                          userText={post.data.selftext}/>
+                          userText={post.data.selftext}
+                          ups={post.data.ups}
+                          hasMedia={post.data.media}
+                          isVideo={post.data.isVideo}
+                          postHint={post.data.post_hint}
+                    />
                 </div>
             ))}
         </div>
